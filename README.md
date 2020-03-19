0c76a40c05e17f13837c4774cabab2305625f4ff
stringang <14845458+stringang@users.noreply.github.com>
2020-03-19 19:01:41 +0800

    WIP

---
+++ README.md
@@ -0,0 +1,6 @@
+# d3js canvas example
+
+- 使用 D3 画出虚拟 DOM
+- 根据 DOM 元素的坐标使用 canvas 画出图形
+
+## Reference
--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,29 @@
+/**
+ * 数据绑定
+ * @param data
+ */
+function databind(data) {
+  var customBase = document.createElement('custom');
+  var custom = d3.select(customBase);
+  var join = custom.selectAll('rect').data(data).enter();
 
-
-
-function databind() {
-
+  console.log(join);
 }
 
-function draw() {
+/**
+ * draw chart
+ * - 坐标原点
+ */
+function draw(context, histogram) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var elements = d3.selectAll('custom.rect');
 
+  elements.each(function(d,i) {
+    var node = d3.select(this);
+    context.fillStyle = node.attr('fillStyle');
+
+    context.fillRect(node.attr('x'), node.attr('y'),
+      node.attr('width'), node.attr('height'))
+  });
+  context.closePath();
 }
